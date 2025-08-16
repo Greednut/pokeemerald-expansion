ad716c47dbc3f21cf2384989321ca7668dffe869
IGitNo <stefanozen2207@gmail.com>
2025-08-16 17:34:44 +0200

    Cameloductor line Learnset

--- src/data/pokemon/egg_moves.h
+++ src/data/pokemon/egg_moves.h
@@ -6366,6 +6366,16 @@ static const u16 sMogeriaEggMoveLearnset[] = {
     MOVE_UNAVAILABLE,
 };
 
+static const u16 sJerminEggMoveLearnset[] = {
+    MOVE_TOXIC_SPIKES,
+    MOVE_DEFEND_ORDER,
+    MOVE_PUNISHMENT,
+    MOVE_ASSIST,
+    MOVE_BATON_PASS,
+    MOVE_DISABLE,
+    MOVE_UNAVAILABLE,
+};
+
 static const u16 sPedestoneEggMoveLearnset[] = {
     MOVE_SHORE_UP,
     MOVE_ACCELEROCK,
@@ -6510,12 +6520,22 @@ static const u16 sTarantagonEggMoveLearnset[] = {
 };
 
 static const u16 sHybrotamusEggMoveLearnset[] = {
-    //MOVE_CRITICAL_MASS,
     MOVE_HEAD_CHARGE,
-    MOVE_METAL_BURST,
-    MOVE_FLARE_BLITZ,
     MOVE_HEAVY_SLAM,
+    MOVE_METAL_BURST,
+    MOVE_SLACK_OFF,
+    MOVE_QUICK_ATTACK,
     MOVE_ZAP_CANNON,
+    MOVE_UNAVAILABLE,
+};
+
+static const u16 sCameloductorEggMoveLearnset[] = {
+    MOVE_BARRIER,
+    MOVE_DRAGON_HAMMER,
+    MOVE_THUNDEROUS_KICK,
+    MOVE_FIRE_SPIN,
+    MOVE_BURN_UP,
+    MOVE_INFERNO,
     MOVE_UNAVAILABLE,
 };
 
